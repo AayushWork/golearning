9379a6c2aca674d482255678adb5b19d8865fd95
AayushWork <aayushwork0812@gmail.com>
2020-08-27 14:17:45 +0530

    after git clone

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world!")
 	fmt.Println("Checking 2nd commit to the same file on github")
+	fmt.Println("Checking 3rd commit to the same file on github")
 }
